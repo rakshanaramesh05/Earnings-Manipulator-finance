b3914df88f7af5297e8e9b15afd13d2bde1c873e
rakshanaramesh05 <rakshanaramesh05@gmail.com>
2026-01-05 02:20:18 +0530

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,15 @@
-app.py
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="Earnings Manipulator App")
+
+st.title("Earnings Manipulation Analyzer")
+
+st.write("App is running successfully 🎉")
+
+uploaded_file = st.file_uploader("Upload Excel file", type=["xlsx"])
+
+if uploaded_file is not None:
+    df = pd.read_excel(uploaded_file)
+    st.subheader("Uploaded Data")
+    st.dataframe(df)
